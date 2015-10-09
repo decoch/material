818f3a5a352a74cd4097e4186e54e6a435641abb
Akifumi.Tominaga <tominaga.switch@gmail.com>
2015-10-09 21:49:04 +0900

    done issue 3-1. 3-2

--- workspace/analysis.py
+++ workspace/analysis.py
@@ -70,5 +70,78 @@ day_counts_tokyo_pie_graph.plot(kind='pie',autopct='%.2f',figsize=(6,6),startang
 
 day_counts_fukuoka_pie_graph = pd.Series(day_counts.values[0], index=day_counts.columns.tolist(),name='福岡の天気の移り変わりの割合グラフ')
 day_counts_fukuoka_pie_graph.plot(kind='pie',autopct='%.2f',figsize=(6,6),startangle=90)
+
+
+## 下記の設問は、「数値要約」と「分布と傾向の把握」に関するスキルを確認するものである。
+# 各都市について、「平均気温」の最小値、25%点、中央値、平均値、75%点、最大値 をそれぞれ求めよ。
+data = pd.read_csv("weather_2012_2014.csv")
+
+sapporo_data = data[data['都市'] == '札幌']
+sapporo_temperature = sapporo_data['平均気温']
+tokyo_data = data[data['都市'] == '東京']
+tokyo_temperature = tokyo_data['平均気温']
+fukuoka_data = data[data['都市'] == '福岡']
+fukuoka_temperature = fukuoka_data['平均気温']
+
+# 福岡
+fukuoka_data.min()
+print "1Q x:" + str(stats.scoreatpercentile(fukuoka_temperature, 25)) #第1四分位
+print "Med x: " + str(numpy.median(fukuoka_temperature)) #中央値
+print "3Q x:" + str(stats.scoreatpercentile(fukuoka_temperature, 75)) #第3四分位
+fukuoka_data.max()
+# 東京
+tokyo_data.min()
+print "1Q x:" + str(stats.scoreatpercentile(tokyo_temperature, 25)) #第1四分位
+print "Med x: " + str(numpy.median(tokyo_temperature)) #中央値
+print "3Q x:" + str(stats.scoreatpercentile(tokyo_temperature, 75)) #第3四分位
+tokyo_data.max()
+# 札幌
+sapporo_data.min()
+print "1Q x:" + str(stats.scoreatpercentile(sapporo_temperature, 25)) #第1四分位
+print "Med x: " + str(numpy.median(sapporo_temperature)) #中央値
+print "3Q x:" + str(stats.scoreatpercentile(sapporo_temperature, 75)) #第3四分位
+sapporo_data.max()
+
+# 各都市の平均気温の推移を比較すため、横軸を日付、縦軸を平均気温としてわかりや すくグラフ化せよ
+
+df['日付'] = fukuoka_data['日付']
+temperature_all = pd.DataFrame(
+    {'日付': fukuoka_data['日付'],
+    '福岡の平均気温':fukuoka_data['平均気温']
+    }
+)
+tokyo_temperature.index = fukuoka_temperature.index
+temperature_all['東京の平均気温'] = tokyo_temperature
+
+sapporo_temperature.index = fukuoka_temperature.index
+temperature_all['東京の平均気温'] = sapporo_temperature
+temperature_all.plot()
+
+temperature_all_box = []
+temperature_all_box.append(temperature_all['福岡の平均気温'])
+temperature_all_box.append(temperature_all['東京の平均気温'])
+temperature_all_box.append(temperature_all['札幌の平均気温'])
+
+data = []
+data.append(temperature_all['福岡の平均気温'])
+data.append(temperature_all['東京の平均気温'])
+data.append(temperature_all['札幌の平均気温'])
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+
+ax.grid() # グリッド線書いてるだけ
+bp = ax.boxplot(data) # 箱ひげ図を書く
+ax.set_ylim([-20, 40]) # y軸の範囲指定
+ax.set_xticklabels(['Fukuoka','Tokyo', 'Sapporo']) # xlabelの設定
+plt.title('Average Temperature')
 plt.show()
-plt.savefig("image.png")
+
+plt.title('Violin Plot of Average Temperature')
+plt.xlabel("都市")
+plt.ylabel("温度")
+violinplot(data, points=20,showmeans=True, showextrema=True, showmedians=True)
+
+
+
+
